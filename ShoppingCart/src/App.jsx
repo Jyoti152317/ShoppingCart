592db1250d9a2cac4a6ad9aa4094146944b54543
Jyoti152317 <engjyoti18@gmail.com>
2025-11-21 14:36:16 +0530

    tailwind setup

--- ShoppingCart/src/App.jsx
+++ ShoppingCart/src/App.jsx
@@ -1,10 +1,24 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+// import "./App.css";
+import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
+import Product from "./components/Product";
+import Cart from "./components/Cart";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Product />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </main>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- ShoppingCart/src/components/Navbar.jsx
+++ ShoppingCart/src/components/Navbar.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex items-center justify-between px-6 py-4 bg-black shadow-md">
+      {/* Title */}
+      <h1 className="text-xl font-bold text-white">Shopping Cart</h1>
+
+      {/* Links */}
+      <div className="flex gap-6">
+        <Link
+          to="/"
+          className="text-white hover:text-blue-600 transition font-medium"
+        >
+          Home
+        </Link>
+
+        <Link
+          to="/cart"
+          className="text-white hover:text-blue-600 transition font-medium"
+        >
+          Cart
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
